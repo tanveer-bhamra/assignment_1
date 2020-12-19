cb25cdfe9f68f267424d268fb77e3987344e2ad8
Titilayo112 <titilayo.ayeni.20@ucl.ac.uk>
2020-12-19 16:13:21 +0000

    Error Handling added to User Input function

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 from shapely.geometry import Polygon
 from shapely.geometry import Point
 
+
 # Task 1 User Input
 
+def user_input():
+    while True:
+        try:
+            x_point = float(input("Enter an Easting coordinate value: "))
+            y_point = float(input("Enter a Northing coordinate : "))
+            user_loc = Point(x_point, y_point)
+            island_mbr: Polygon = Polygon([(430000, 95000), (430000, 80000), (465000, 80000), (465000, 95000)])
+            break
+        except:
+            print('Please enter a valid Easting and Northing ')
+            user_input()
+            
+    if user_loc.within(island_mbr):
+        print('inside')
+    else:
+        print("Not within the software's boundary\nBoundary extends: \n 430000 - 465000 East\n 80000 - 95000 North"
+              " \n Please input a new coordinates\n\n ")
+        user_input()
+        # quit software
+    return user_loc, print('within boundary')  # remove the print when program works
 
-# can add error handling using while loop , and gate, try and except, with a recursion
-x_point = float(input("Enter an Easting coordinate value: "))
-y_point = float(input("Enter a Northing coordinate : "))
 
-user_loc = Point(x_point, y_point)
-island_mbr = Polygon([(430000, 95000), (430000, 80000), (465000, 80000), (465000, 95000)])
-
-if user_loc.within(island_mbr):
-    pass
-else:
-    print('Not within the softwares boundary ')
-    # recursion to add to the function
+point = user_input()
+# Task 2
