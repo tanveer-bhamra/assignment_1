5c6bfaa36f17a753fa53ea928595ca5a5723efef
Tanveer_Singh <tanveer.bhamra.17@ucl.ac.uk>
2020-12-22 15:56:17 +0100

    created window for elevation raster

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+# import modules
+
 from shapely.geometry import Polygon
 from shapely.geometry import Point
+import rasterio
+from rasterio import features
+from rasterio.windows import Window
+import numpy as np
+
 
 
 # Task 1 User Input
@@ -27,4 +34,31 @@ def user_input():
 
 
 point = user_input()
-# Task 2
+
+# Task 2 Highest Point Identification - Identify the highest point within a 5km radius from the user location.
+
+# read in elevation data file
+
+elevation_file = rasterio.open("Material/elevation/SZ.asc")
+elevation = elevation_file.read(1)
+elevation_file_bounds = elevation_file.bounds
+
+# define the bounds of the elevation file (this info is given in elevation_file_bounds variable)
+w_lim = 425000
+n_lim = 100000
+s_lim = 75000
+e_lim = 470000
+
+# CREATE A WINDOW
+
+# define row and cloumn offset
+row_off, col_off  = elevation_file.index(w_lim, n_lim)
+
+# define the width and height of the window
+height = elevation_file.height  #rows
+width = elevation_file.width #coloumns
+
+elevation_window = elevation_file.read(1, window=Window(col_off, row_off, width, height))
+
+# print(elevation_window.shape)
+
