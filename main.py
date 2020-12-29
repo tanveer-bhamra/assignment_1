2d3da6f9e51e244b168378a913116660eb3093c9
Tanveer_Singh <tanveer.bhamra.17@ucl.ac.uk>
2020-12-29 16:33:42 +0100

    task4, calculated incorrect traveltime. need to weight network first

--- main.py
+++ main.py
@@ -5,27 +5,33 @@ import rasterio
 from rasterio import features
 from rasterio.windows import Window
 from rasterio import plot
+import matplotlib.pyplot as plt
+import networkx as nx
 import numpy as np
 import json
 import os
 from rtree import index
+from scipy import sparse
 
-# Task 1 - User input
+# # Task 1 - User input
+#
+# x_cord = int(input("please enter a numeric Easting coordinate value between 430000 - 465000: "))
+# while x_cord < 430000 or x_cord > 465000:
+#     x_cord = int(input(
+#         "Entry not in range. Please enter a numeric easting coordinate value between 430000 - 465000: "))
+# else:
+#     y_cord = int(input("please enter a numeric Northing coordinate value between 80000 - 95000: "))
+#     while y_cord < 80000 or y_cord > 95000:
+#         y_cord = int(input(
+#             "Entry not in range. Please enter a numeric Northing coordinate value between 80000 - 95000: "))
+#     else:
+#         print("Your location ", x_cord, " Easting and ", y_cord, " Northing is within the northing and easting "
+#                                                                  "bounds ")
+#
+# point = Point(x_cord, y_cord)
 
-x_cord = int(input("please enter a numeric Easting coordinate value between 430000 - 465000: "))
-while x_cord < 430000 or x_cord > 465000:
-    x_cord = int(input(
-        "Entry not in range. Please enter a numeric easting coordinate value between 430000 - 465000: "))
-else:
-    y_cord = int(input("please enter a numeric Northing coordinate value between 80000 - 95000: "))
-    while y_cord < 80000 or y_cord > 95000:
-        y_cord = int(input(
-            "Entry not in range. Please enter a numeric Northing coordinate value between 80000 - 95000: "))
-    else:
-        print("Your location ", x_cord, " Easting and ", y_cord, " Northing is within the northing and easting "
-                                                                 "bounds ")
-
-point = Point(x_cord, y_cord)
+# Practice Point
+point = Point(439000, 85000)
 
 # # test point to copy and paste   435000,85000
 #
@@ -102,8 +108,9 @@ itn_json = os.path.join("Material/itn/solent_itn.json")
 with open(itn_json, 'r') as f:
     itn_json = json.load(f)
 
+road_links = itn_json['roadlinks']
 road_nodes = itn_json['roadnodes']
-node_coord = []
+
 
 # initialize rtree
 idx = index.Index()
@@ -118,23 +125,139 @@ for i, (coord_id, coords) in enumerate(road_nodes.items()):
         idx.insert(i, (node_co[0], node_co[1], node_co[0], node_co[1]), coord_id)
 
 # Query to find  the nearest fid for the users point
-# fid for  user test point osgb4000000026141631
 for i in idx.nearest((point.x,point.y), 1):
-    user_node = id_list[i]
-    print(user_node)
+    start_node_name = id_list[i]
 
 # Query to find  the nearest fid for the highest point
-# fid for highest test point osgb4000000026227672
 
-for i in idx.nearest((highest_point_cord), 1):
-    highest_node = id_list[i]
-    print(highest_node)
+for i in idx.nearest(highest_point_cord, 1):
+    dest_node_name = id_list[i]
 
 # Task  4:  Shortest  Path
 
-# load Json road links
-# load json roadnodes
-# create network graph
+# Determine difference in height
+
+start_node_coords = road_nodes[start_node_name]['coords']
+dest_node_coords = road_nodes[dest_node_name]['coords']
+
+start_node_array_coords = elevation_file.index(start_node_coords[0], start_node_coords[1])
+dest_node_array_coords = elevation_file.index(dest_node_coords[0], dest_node_coords[1])
+
+start_node_elevation = clipped_elevation_matrix[start_node_array_coords[0], start_node_array_coords[1]]
+dest_node_elevation = clipped_elevation_matrix[dest_node_array_coords[0], dest_node_array_coords[1]]
+delta_elevation = dest_node_elevation - start_node_elevation
+
+# Instantiate graph
+
+# g = nx.DiGraph()
+g = nx.Graph()
+
+# Add road lines
+
+for edge in road_links:
+    g.add_edge(road_links[edge]['start'], road_links[edge]['end'], 
+               weight=road_links[edge]['length'])
+
+# Dijkstra
+
+path = nx.dijkstra_path(g, source=start_node_name, target=dest_node_name,
+                        weight='weight')
+
+# Network Length and time to traverse
+
+#print('gnodes: ', g.nodes)
+#print('gedges: ', g.edges)
+
+# create a list (length_between_each_node) where each item is the length 
+#between each node along the path
+
+number_of_nodes_on_path = len(path)
+length_between_each_node =[]
+for i in range(1,number_of_nodes_on_path):
+    length_between_each_node.append(g[path[i-1]][path[i]])
+    
+print(length_between_each_node)
+
+length_between_each_node = [i["weight"] for i in length_between_each_node]
+print(length_between_each_node)
+network_length = sum(length_between_each_node)
+print(network_length)
+
+# cretae a list of elevations corresponding to each node along the path
+# create empty list
+elevation_of_each_node = []
+
+# Get BNG coordinates of eachnode along path
+
+# find where the BNG coordinates of each node are in the numpy array
+node_coords = []
+for i in range(len(path)):
+    node_coords.append(road_nodes[path[i]]['coords'])
+    
+# find the corresponding elevation indexes of these BNG coordinates and store them
+# in a list called node_elevation_index
+
+node_elevation_index = []
+for i in range(len(path)):
+    node_elevation_index.append( elevation_file.index(node_coords[i][0], node_coords[i][1]))
+  
+# find the elevation values of the indexes
+
+node_elevation = []
+
+for  i in range(len(path)):
+    node_elevation.append(clipped_elevation_matrix[node_elevation_index[i][0], 
+                          node_elevation_index[i][1]])
+    
+# calculate difference in elevation between 2 condectuvive nodes in node_elevation
+# variable
+delta_node_elevation = []
+for i in range(1,len(path)):
+    delta_node_elevation.append(node_elevation[i] - node_elevation[i-1])
+delta_node_elevation = list(np.around(np.array(delta_node_elevation),2))
+
+# calculate base time (no elevation included)
+speed_ms = 1.38888 # unit metres/second
+base_time = []
+
+for i in range(len(length_between_each_node)):
+    base_time.append(length_between_each_node[i]/speed_ms)
+
+
+# apply naismith rule to calculate additional traveltime between each node
+
+additional_time = []
+
+for i in range(len(delta_node_elevation)):
+    if delta_node_elevation[i] > 0:
+        additional_time.append((delta_node_elevation[i]/0.01)*0.06)
+    else:
+        additional_time.append(0)
+        
+traveltime = np.add(base_time, additional_time)
+print(traveltime)
+
+
+# printing results
+# print('starting node: ', start_node_name)
+# print('destination node: ', dest_node_name)
+# print('total network length: ', network_length)
+# print('delta elevation: ', delta_elevation)
+print('Dijkstra Path: ', path)
+print(g[path[0]][path[1]])
+
+rasterio.plot.show(clipped_elevation_matrix)
+
+
+# REFERENCES:
+
+# https://numpy.org/doc/stable/reference/generated/numpy.argmax.html
+# https://rasterio.readthedocs.io/en/latest/api/rasterio.transform.html
+  
+
+
+
+
 
 # REFERENCES:
 
