d30a987927a583e755961619d5e69f395fda3889
Tanveer_Singh <tanveer.bhamra.17@ucl.ac.uk>
2021-01-05 17:23:31 +0100

    task 6 - extended region completed

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from shapely.geometry import Polygon
 from shapely.geometry import Point
 from shapely.geometry import LineString
 import geopandas as gpd
-import matplotlib.pyplot as plt
 from cartopy import crs
 import rasterio
 from rasterio import features
@@ -17,33 +16,85 @@ import numpy as np
 import json
 import os
 from rtree import index
-from scipy import sparse
 import cartopy.crs as ccrs
-import cartopy
-from matplotlib_scalebar.scalebar import ScaleBar
+import sys
+
+
+class Buffer:
+    def __init__(self, input_point, radius, matrix, affine_transformation):
+        self.buffer = input_point.buffer(radius)
+        self.inv_bool = ~rasterio.features.rasterize([(self.buffer, 1)], out_shape=elevation_shape,
+                                                     transform=affine_transformation).astype(bool)
+        self.matrix = matrix
+        self.matrix[self.inv_bool] = np.nan
+        self.maximum_elevation = []
+        # Add this^ value to the printed map
+        self.maximum_elevation_coords = ()
+
+    def highest_point(self):
+        row_idx, col_idx = np.unravel_index(np.nanargmax(self.matrix, ),
+                                            elevation_shape)
+        self.maximum_elevation.append(elevation_matrix[row_idx, col_idx])
+        x_max_cord, y_max_cord = elevation_file.xy(row_idx, col_idx)
+        self.maximum_elevation_coords = (x_max_cord, y_max_cord)
+
 
 # # Task 1 - User input
 
-x_cord = int(input("please enter a numeric Easting coordinate value between 430000 - 465000: "))
-while x_cord < 430000 or x_cord > 465000:
-    x_cord = int(input(
-        "Entry not in range. Please enter a numeric easting coordinate value between 430000 - 465000: "))
-else:
-    y_cord = int(input("please enter a numeric Northing coordinate value between 80000 - 95000: "))
-    while y_cord < 80000 or y_cord > 95000:
-        y_cord = int(input(
-            "Entry not in range. Please enter a numeric Northing coordinate value between 80000 - 95000: "))
-    else:
-        print("Your location ", x_cord, " Easting and ", y_cord, " Northing is within the northing and easting "
-                                                                  "bounds ")
+# x_cord = int(input("please enter a numeric Easting coordinate value between 430000 - 465000: "))
+# while x_cord < 430000 or x_cord > 465000:
+#     x_cord = int(input(
+#         "Entry not in range. Please enter a numeric easting coordinate value between 430000 - 465000: "))
+# else:
+#     y_cord = int(input("please enter a numeric Northing coordinate value between 80000 - 95000: "))
+#     while y_cord < 80000 or y_cord > 95000:
+#         y_cord = int(input(
+#             "Entry not in range. Please enter a numeric Northing coordinate value between 80000 - 95000: "))
+#     else:
+#         print("Your location ", x_cord, " Easting and ", y_cord, " Northing is within the northing and easting "
+#                                                                  "bounds ")
+        
+# TASK 6 - EXTENDED ENVIRONMENT
 
+#input BNG easting and northing coordinates
+x_cord = float(input("please enter a numeric Easting coordinate value: "))
+y_cord = float(input("please enter a numeric Northing coordinate value: "))
+
+
+# #Practice Point
+# point = Point(439000, 85000) # inside polygon
+#point = Point(439000, 85000) # inside polygon
+#point = Point(435467.0003706848, 89877.89764861623) # basic boundary point test
+#point = Point(450570.5006727509, 96489.40075877355) #boundary point test northern river. 
+#point = Point(458703.8009043259, 79878.203815303283) #boundary point test
+#point = Point(442533.5998012663, 92429.09739499213) # boundary point river
+#point = Point(430037.8998216286, 85012.30020408472) # boundary point
+#point = Point(445379.6009559168, 78917.60170240863)
+#point = Point(433797.3990891299, 85464.4023485058) # another very high point is much closer
+
+#read in the isle of wight shapefile
+gdf = gpd.read_file('C:/Users/tanny/OneDrive/Documents/university year 4/Geospatial Programming/group-assignment/Material/shape/isle_of_wight.shp')
+
+#checking correct CRS is used
+gdf = gdf.to_crs(epsg=27700)
+
+# return a linestring geoemtry of the boundary/polygon of the isle of wight
+isle_of_wight_polygon = (gdf.loc[0,'geometry'])
+
+# create point object of user input
 point = Point(x_cord, y_cord)
 
-# Practice Point
-#point = Point(439000, 85000)
+# testing to see if inputted points are within, outside or on the border of the isle of wight
+if point.touches(isle_of_wight_polygon):
+    print('Your location is on the border of the isle of wight.')
+elif point.within(isle_of_wight_polygon):
+    print('Your location is inside the isle of wight.')
+else:
+    print('Your location is outside the isle of wight.')
+    # terminates program
+    sys.exit("your inputted coordinates are not within the isle of wight. This program will now terminate.")
 
-# # test point to copy and paste   435000,85000
-#
+
 # Task 2 Highest Point Identification - Identify the highest point within a 5km radius from the user location.
 
 # read in elevation_matrix data file
@@ -59,55 +110,16 @@ n_lim = 100000
 s_lim = 75000
 e_lim = 470000
 
-# create a 5km buffer around the inputted point object
-
-buffer = point.buffer(5000)
-
 # apply affine transformation to map pixel/cell locations to spatial positions
 
 affine_tr = rasterio.transform.from_bounds(w_lim, s_lim,
                                            e_lim, n_lim,
                                            elevation_shape[1], elevation_shape[0])
 
-# Converting a buffer vector geometry into raster  where cells are ones (inside 5km buffer)
-# or zeros (outside 5km buffer)
-
-buffer_raster = rasterio.features.rasterize([(buffer, 1)], out_shape=elevation_shape,
-                                            transform=affine_tr)
-
-# convert buffer matrix into boolean data type
-buffer_bool = buffer_raster.astype(bool)
-
-# invert buffer_bool matrix
-buffer_bool_inv = ~buffer_bool
-
-# clip elevation_matrix array using buffer array
-clipped_elevation_matrix = elevation_matrix.copy()
-clipped_elevation_matrix[buffer_bool_inv] = np.nan
-
-# find maximum height in the buffer
-
-row_idx, col_idx = np.unravel_index(np.nanargmax(clipped_elevation_matrix, ),
-                                    elevation_shape)
-
-# return highest point in meters in 5km buffer region
-
-max_elevation = elevation_matrix[row_idx, col_idx]
-
-# retrieve  the British national grid coordinates of the pixel containing the maximum elevation_matrix inside the buffer region
-
-x_max_cord, y_max_cord = elevation_file.xy(row_idx, col_idx)
-
-# create a point object  and list of highest point inside 5km buffer
-
-highest_point = Point(x_max_cord, y_max_cord)
-highest_point_cord = [x_max_cord, y_max_cord]
+five_km = Buffer(point, 5000, elevation_matrix, affine_tr)
+five_km.highest_point()
 
 # TASK 3 - Nearest Integrated Transport Network
-# test point to copy and paste   435000,85000
-# highest point for test point  439007.5 85192.5 #( test points can be deleted when completed)
-# highest_point_cord = [439007.5, 85192.5] #( test points can be deleted when completed)
-# point = Point(435000, 85000)  #( test points can be deleted when completed)
 
 # load itn file
 itn_json = os.path.join("Material/itn/solent_itn.json")
@@ -126,7 +138,7 @@ for i, (coord_id, coords) in enumerate(road_nodes.items()):
     node_co = (coords['coords'])
     q_node_co = Point(node_co[0], node_co[1])
     id_list.append(coord_id)
-    if buffer.contains(q_node_co):
+    if five_km.buffer.contains(q_node_co):
         idx.insert(i, (node_co[0], node_co[1], node_co[0], node_co[1]), coord_id)
 
 # Query to find  the nearest fid for the users point
@@ -135,51 +147,48 @@ for i in idx.nearest((point.x, point.y), 1):
 
 # Query to find  the nearest fid for the highest point
 
-for i in idx.nearest(highest_point_cord, 1):
+for i in idx.nearest(five_km.maximum_elevation_coords, 1):
     dest_node_name = id_list[i]
+
+# Printing starting and destination nodes, error handling
+
+try:
+    print('Nearest node: ', start_node_name)
+    print('Destination node: ', dest_node_name)
+except NameError:
+    print('No nodes within buffer, please restart with different coordinates')
+    exit()
 
 # Task  4:  Shortest  Path
 
-# Determine difference in height
+# Instantiate graph
 
 g = nx.Graph()
 
-# Add edges road lines using road_links data
+# Add edges using road_links data to the graph
 
 for edge in road_links:
     g.add_edge(road_links[edge]['start'], road_links[edge]['end'], fid=edge, length=road_links[edge]['length'])
 
-# Calculate the amount of time it would take to walk the edge, irrespective of elevation at 5km/h
+# Calculate the amount of time it would take to walk each edge, irrespective of elevation, at 5km/h
 
 for u, v in g.edges:
-    g.edges[u, v]['base_time'] = (g.edges[u, v]['length'])/1.38889
+    g.edges[u, v]['base_time'] = (g.edges[u, v]['length']) / 1.38889
 
-# Calculate the change in elevation along each edge
-expanded_buffer = point.buffer(7500)
+# Create a buffer slightly larger than the 5km radius, in case the dijkstra path involves briefly leaving the 5km buffer
 
-# Converting a buffer vector geometry into raster  where cells are ones (inside 5km buffer)
-# or zeros (outside 5km buffer)
+seven_km = Buffer(point, 7000, elevation_matrix, affine_tr)
 
-expanded_buffer_raster = rasterio.features.rasterize([(buffer, 1)], out_shape=elevation_shape,
-                                            transform=affine_tr)
-
-# convert buffer matrix into boolean data type
-expanded_buffer_bool = expanded_buffer_raster.astype(bool)
-
-# invert buffer_bool matrix
-expanded_buffer_bool_inv = ~expanded_buffer_bool
-
-expanded_buffer_search = elevation_matrix.copy()
-expanded_buffer_search[buffer_bool_inv] = np.nan
+# Calculate the change in elevation for each node
 
 for u, v in g.edges:
     u_node_coords = road_nodes[u]['coords']
     u_node_array_coords = elevation_file.index(u_node_coords[0], u_node_coords[1])
-    u_node_elevation = expanded_buffer_search[u_node_array_coords[0], u_node_array_coords[1]]
+    u_node_elevation = seven_km.matrix[u_node_array_coords[0], u_node_array_coords[1]]
 
     v_node_coords = road_nodes[v]['coords']
     v_node_array_coords = elevation_file.index(v_node_coords[0], v_node_coords[1])
-    v_node_elevation = expanded_buffer_search[v_node_array_coords[0], v_node_array_coords[1]]
+    v_node_elevation = seven_km.matrix[v_node_array_coords[0], v_node_array_coords[1]]
 
     delta_elevation = v_node_elevation - u_node_elevation
     g.edges[u, v]['delta_elevation'] = delta_elevation
@@ -194,28 +203,21 @@ for u, v in g.edges:
     total_time = g.edges[u, v]['base_time'] + naismith_time
     g.edges[u, v]['total_time'] = total_time
 
-# print(g.edges(data=True))
+# Determine Dijkstra (shortest) path
 
-# Dijkstra
-#
 path = nx.dijkstra_path(g, source=start_node_name, target=dest_node_name, weight='total_time')
 
-# printing results
-print('starting node: ', start_node_name)
-print('destination node: ', dest_node_name)
-print('Dijkstra Path: ', path)
-print('Example of edge attributes:')
-print(g[path[6]][path[7]])
+# create a starting point (first node in ITN)
 
-# create a starting point (first node in ITN) 
 start_node_coords = road_nodes[path[0]]['coords']
 start_point = Point(start_node_coords[0], start_node_coords[1])
-#create a destination point (last node in index)
+
+# create a destination point (last node in index)
+
 end_node_coords = road_nodes[path[-1]]['coords']
 destination_point = Point(end_node_coords[0], end_node_coords[1])
 
 # STEP5 - PLOTTING MAPS AND DATA
-
 
 # read in raster-50k_2724246.tif file as map_tif
 
@@ -226,43 +228,43 @@ map_tif = rasterio.open("Material/background/raster-50k_2724246.tif")
 width_of_map = 10000
 height_of_map = 10000
 
-western_map_edge, eastern_map_edge = point.x - (width_of_map/2), point.x + (width_of_map/2)
-southern_map_edge, northern_map_edge = point.y - (height_of_map/2), point.y + (height_of_map/2)
+western_map_edge, eastern_map_edge = point.x - (width_of_map / 2), point.x + (width_of_map / 2)
+southern_map_edge, northern_map_edge = point.y - (height_of_map / 2), point.y + (height_of_map / 2)
 
-#identify rows and coloumns of the pixels of the NW and SE corner in the background map
+# identify rows and coloumns of the pixels of the NW and SE corner in the background map
 row_west, col_north, = map_tif.index(western_map_edge, northern_map_edge)
-row_east, col_south  = map_tif.index( eastern_map_edge, southern_map_edge)
+row_east, col_south = map_tif.index(eastern_map_edge, southern_map_edge)
 
-#define map height and width
-map_height = row_east - row_west +10
-map_width = col_south - col_north +10
+# define map height and width
+map_height = row_east - row_west + 10
+map_width = col_south - col_north + 10
 # print(map_width)
 # print(map_height)
 
 # create window for background map
-window_map = map_tif.read(1, window=Window(col_north, row_west, 
+window_map = map_tif.read(1, window=Window(col_north, row_west,
                                            map_width, map_height))
 
 # create and cut a window for the elevation map
 
-# identify rows and columns of the pixels (in the eleavtion array) 
-# row and column of NW corner 
+# identify rows and columns of the pixels (in the eleavtion array)
+# row and column of NW corner
 row_west1, col_north1 = elevation_file.index(western_map_edge, northern_map_edge)
 # row and column of SE corner
 row_east1, col_south1 = elevation_file.index(eastern_map_edge, southern_map_edge)
 
-#width and height of window
-elevation_height = row_east1 - row_west1 +10
+# width and height of window
+elevation_height = row_east1 - row_west1 + 10
 elevation_width = col_south1 - col_north1 + 10
- 
+
 # BNG coordinates of NW corner.
 elevation_west, elevation_north = elevation_file.xy(row_west1,
-                                                 col_north1,
-                                                 offset='ul')
+                                                    col_north1,
+                                                    offset='ul')
 # BNG coordinates of SE corner.
-elevation_east, elevation_south = elevation_file.xy(row_west1+elevation_height,
-                                                 col_north1+elevation_width,
-                                                 offset='ul')
+elevation_east, elevation_south = elevation_file.xy(row_west1 + elevation_height,
+                                                    col_north1 + elevation_width,
+                                                    offset='ul')
 # create a  list of the N,W,S,E extents
 elevation_extents = [elevation_west,
                      elevation_east,
@@ -270,15 +272,15 @@ elevation_extents = [elevation_west,
                      elevation_north]
 
 # cut the window in the elevation array
-elevation_matrix_cut = elevation_file.read(1, window= Window(col_north1,
-                          row_west1,
-                          elevation_width,
-                          elevation_height))
+elevation_matrix_cut = elevation_file.read(1, window=Window(col_north1,
+                                                            row_west1,
+                                                            elevation_width,
+                                                            elevation_height))
 
 # create a GeoDataFrame of the shortest path
 
-links = [] # this list will be used to populate the feature id (fid) column
-geom  = [] # this list will be used to populate the geometry column
+links = []  # this list will be used to populate the feature id (fid) column
+geom = []  # this list will be used to populate the geometry column
 
 first_node = path[0]
 for node in path[1:]:
@@ -307,53 +309,44 @@ ax.imshow(background_map, origin='upper', extent=background_extents, zorder=0)
 ax.set_extent(background_extents, crs=crs.OSGB())
 
 # plot shortest path on background map
-shortest_path_gpd.plot(ax=ax, edgecolor='blue', linewidth=0.5, zorder=2,label='shortest path')
+shortest_path_gpd.plot(ax=ax, edgecolor='blue', linewidth=0.5, zorder=2, label='shortest path')
 
-#plot user input point
-plt.scatter(point.x, point.y, marker = "x", s=5,label='Your starting point')
-#plot starting point
-plt.scatter(start_point.x, start_point.y, s=5,label='The routes starting point')
+# plot user input point
+plt.scatter(point.x, point.y, marker="x", s=5, label='Your starting point')
+# plot starting point
+plt.scatter(start_point.x, start_point.y, s=5, label='The routes starting point')
 
 # plot ending point
-plt.scatter(destination_point.x, destination_point.y, s=5,label='Destination')
+plt.scatter(destination_point.x, destination_point.y, s=5, label='Destination')
 
-#plot buffer region
-circle1=plt.Circle((point.x,point.y),5000,color='red', fill = False, linewidth=0.5,label='5km Buffer zone')
+# plot buffer region
+circle1 = plt.Circle((point.x, point.y), 5000, color='red', fill=False, linewidth=0.5, label='5km Buffer zone')
 circle_buff = plt.gcf().gca().add_artist(circle1)
 circle_buff.set_label('5km Buffer')
 
-
 # plot elevation array
 elev_img = ax.imshow(elevation_matrix_cut, origin='upper', alpha=0.3,
-          extent=elevation_extents, transform=ccrs.OSGB(), zorder=1)
+                     extent=elevation_extents, transform=ccrs.OSGB(), zorder=1)
 
-#Colour bar for elevation
-colour_bar = plt.colorbar(elev_img,label='Elevation')
+# Colour bar for elevation
+colour_bar = plt.colorbar(elev_img, label='Elevation')
 
 plt.title('Isle of Wight Emergency Route Planner')
 
-#Create North arrow
+# Create North arrow
 box = dict(boxstyle="rarrow,pad=0.3", fc="black", ec="k")
 ax.text((point.x + 4000), (point.y + 4000), "    ", rotation=90,
         size=4,
         bbox=box)
 # 5km scale - change to text fraction
-#plt.annotate(' ',xy=(point.x,(point.y - 4700)), xytext= ((point.x + 5000) ,(point.y - 4700)),
-            # arrowprops= dict(arrowstyle="-"))
-#plt.text(point.x + 2000, (point.y - 4500), '5km')
-# plt.axhline(y=point.y - 4000, xmin=point.x, xmax=point.x +5000)
-
+plt.annotate(' ', xy=(point.x, (point.y - 4700)), xytext=((point.x + 5000), (point.y - 4700)),
+             arrowprops=dict(arrowstyle="-"))
+plt.text(point.x + 2000, (point.y - 4500), '5km')
+plt.axhline(y=point.y - 4000, xmin=point.x, xmax=point.x + 5000)
 plt.legend(loc='best', fontsize=3, bbox_to_anchor=(0.9, -0.02), ncol=3)
-#Scalebar
-scalebar = ScaleBar(0.08,length_fraction=0.25,scale_formatter = lambda value, unit: f"{2.5} {'Km'}",label_loc= 'top',
-                    location='lower right')
-ax.add_artist(scalebar)
-
-
-
 
 plt.show()
-
+plt.savefig('route_to_highest_point.png') #save fig
 
 # https://numpy.org/doc/stable/reference/generated/numpy.argmax.html
 # https://rasterio.readthedocs.io/en/latest/api/rasterio.transform.html
